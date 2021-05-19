0f34f14edd24ac8fa3a38006290a4b29969866ba
Carlos_Dantas <carlosdantas485@gmail.com>
2021-05-19 20:26:00 -0300

    V0.0.1 Star Project

---
+++ src/main/java/com/springboot/ApiSpringbootBankApplication.java
@@ -0,0 +1,13 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootBankApplication.class, args);
+	}
+
+}
